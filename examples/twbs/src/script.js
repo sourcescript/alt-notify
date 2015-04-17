431e7d3d3f3d5104f784bf060187495185be8ec5
srph <seraphipod@gmail.com>
2015-04-18 06:43:52 +0800

    Fixed config export. Added example.

--- examples/twbs/src/script.js
+++ examples/twbs/src/script.js
@@ -1,19 +1,22 @@
+require('babel/polyfill');
 import React from 'react';
-import { Drawer, NotifyActions } from 'alt-notify';
+import { Drawer, NotifyActions } from '../../../';
 
 var App = React.createClass({
-  render: function() {
+  render() {
     return (
       <div>
         <Drawer filter="alert"
           render={(data) => {
-            <div key={data.key}> {data.text} </data>
+            <div key={data.key}> {data.text} </div>
           }} />
 
-        <button onClick={NotifyActions.add.bind(null, { text: 'hehe' }) type="button">
+        <button onClick={NotifyActions.add.bind(null, { text: 'hehe' })} type="button">
           Notify
         </button>
       </div>
     );
   }
 });
+
+React.render(<App />, document.body)
