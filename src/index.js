8a51d49a2ccf042708d22d4e8c9a1c14af521045
srph <seraphipod@gmail.com>
2015-04-19 07:14:10 +0800

    Fixed config export.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export * as config from './config';
+export config from './config';
 export Drawer from './Drawer';
 export NotifyActions from './NotifyActions';
 export NotifyStore from './NotifyStore';
