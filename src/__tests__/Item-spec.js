10c63a7b185304d541c63789aebafb6c030bdab9
Mark Penaranda <markangelpenaranda@gmail.com>
2015-04-26 23:47:58 +0800

    Refactored tests to es6 and sinon sandbox

--- src/__tests__/Item-spec.js
+++ src/__tests__/Item-spec.js
@@ -4,19 +4,22 @@ import NotifyActions from '../NotifyActions';
 import Item from '../Item';
 
 describe('Item object', () => {
-  var clock;
-  beforeEach(function () { clock = sinon.useFakeTimers(); });
-  afterEach(function () { clock.restore(); });
+  var clock, sandbox;
+  beforeEach(() => {
+    sandbox = sinon.sanbox.create();
+    clock = sandbox.useFakeTimers();
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
 
   describe('#constructor', () => {
     it('should start the timer when instantiated', () => {
-      sinon.spy(window, 'setTimeout');
+      sandbox.spy(window, 'setTimeout');
 
       // It's called on the constructor so we'll just instantiate
       var item = new Item();
       expect(window.setTimeout.called).to.equal(true);
-
-      window.setTimeout.restore();
     });
   });
 
@@ -25,39 +28,33 @@ describe('Item object', () => {
   describe('#remove', () => {
     it('should cancel the timer for itself after the provided duration', () => {
       const duration = 1000;
-      sinon.spy(window, 'clearTimeout');
+      sandbox.spy(window, 'clearTimeout');
       // We don't really need to test the `removeHandler`
       // as this is tested on the test below
       var item = new Item({ duration });
 
       clock.tick(duration);
       expect(clearTimeout.called).to.equal(true);
-
-      window.clearTimeout.restore();
     });
 
     it('should call `remove` within the <config defaults> if the <duration> was not provided', () => {
       const duration = 10000;
-      sinon.stub(config, 'duration').returns(duration);
+      sandbox.stub(config, 'duration').returns(duration);
       // We don't really need to test the `removeHandler`
       // as this is tested on the test below
-      sinon.spy(Item.prototype, 'remove');
+      sandbox.spy(Item.prototype, 'remove');
       var item = new Item({});
 
       clock.tick(duration);
       expect(item.remove.called).to.equal(true);
-
-      Item.prototype.remove.restore();
-      config.duration.restore();
     });
 
     it('should call the NotifyActions.remove with the given id', () => {
-      var spy = sinon.spy(NotifyActions, 'remove');
+      var spy = sandbox.spy(NotifyActions, 'remove');
       var item = new Item({});
 
       item.remove();
       expect(NotifyActions.remove.called).to.equal(true);
-      NotifyActions.remove.restore()
     });
   });
 });
